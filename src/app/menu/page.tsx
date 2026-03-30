a7013dda38436c5823df950a5a5f6cb0a8b168d2
yeabsira458 <yabewtk@gmail.com>
2026-03-30 16:26:02 +0300

    Food ordering Full Stack

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -4,18 +4,27 @@ import React from "react";
 
 const MenuPage = () => {
   return (
-    <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
+    <div className="p-4 lg:px-20 xl:px-40 min-h-[calc(100vh-6rem)] md:min-h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center bg-white">
       {menu.map((category) => (
         <Link
           href={`/menu/${category.slug}`}
           key={category.id}
-          className="w-full h-1/3 bg-cover p-8 md:h-1/2"
+          className="w-full h-1/3 md:h-[60vh] flex-1 bg-cover p-8 transition-all duration-300 hover:opacity-90"
           style={{ backgroundImage: `url(${category.img})` }}
         >
-          <div className={`text-${category.color} w-1/2`}>
+          {/* We use inline styles for the color because Tailwind can't guess dynamic variables */}
+          <div style={{ color: category.color }} className="w-1/2">
             <h1 className="uppercase font-bold text-3xl">{category.title}</h1>
-            <p className="text-sm my-8">{category.desc}</p>
-            <button className={`hidden 2xl:block bg-${category.color} text-${category.color === "black" ? "white" : "red-500"} py-2 px-4 rounded-md`}>Explore</button>
+            <p className="text-sm my-8 font-semibold">{category.desc}</p>
+            <button
+              style={{
+                backgroundColor: category.color,
+                color: category.color === "black" ? "white" : "red",
+              }}
+              className="hidden 2xl:block py-2 px-4 rounded-md font-bold"
+            >
+              Explore
+            </button>
           </div>
         </Link>
       ))}
